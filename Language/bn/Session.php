797668483066206e6b9ddbb31b8700cfb4e3edb2
webdevron <mbpron@gmail.com>
2020-01-04 23:58:24 +0600

    View and Session file completed

---
+++ Language/bn/Session.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Session language strings.
+ *
+ * @package    CodeIgniter
+ * @author     M. B. Parvez (WebDevRon)
+ * @copyright  2019 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'missingDatabaseTable'   => 'ডেটাবেজ সেশন হ্যান্ডেলারের সঠিক ভাবে কাজ করার জন্য `sessionSavePath` এ টেবিলের নাম অবশ্যক',
+   'invalidSavePath'        => 'সেশন: সংরক্ষন করার জন্য কনফিগার করা সেশন পথ "{0}" কোন ডিরেক্টরি নয়, এমন কোন ডেরেক্টরি নেই অথবা তৈরি করা সম্ভব নয়।',
+   'writeProtectedSavePath' => 'সেশন: সংরক্ষন করার জন্য কনফিগার করা সেশন পথ "{0}" পিএইচপি প্রোসেস (PHP process) দ্বারা রাইট করা সম্ভব নয়',
+   'emptySavePath'          => 'সেশন: সংরক্ষন করার জন্য সেশন ডিরেক্টরি পথ কনফিগার করা হয়নি',
+   'invalidSavePathFormat'  => 'সেশন: সংরক্ষন করার জন্য {0} ডিরেক্টরিটি ভুল',
+];
---
+++ Language/bn/View.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * View language strings.
+ *
+ * @package    CodeIgniter
+ * @author     M. B. Parvez (WebDevRon)
+ * @copyright  2019 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'invalidCellMethod'     => '{class}::{method} মেথডটি সঠিক নয়।',
+   'missingCellParameters' => '{class}::{method} এর কোন প্যারামিটার নেই।',
+   'invalidCellParameter'  => '{0} সঠিক কোন প্যারামিটারের নাম নয়।',
+   'noCellClass'           => 'কোন ভিউ সেল ক্লাস (view cell class) প্রদান করা হয়নি।',
+   'invalidCellClass'      => '{0} - এই ভিউ সেল ক্লাস (view cell class) সনাক্ত করা সম্ভব হয়নি।',
+   'tagSyntaxError'        => '{0} এই পার্সার ট্যাগ (Parser tags) এর চিহ্নতে ভুল (syntax error) আছে।'
+];
